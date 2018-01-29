6f2efa0470f08e665bbac3ffe15562b4e5ad3d5e
FNChange1996 <2772002035@qq.com>
2018-01-29 17:56:15 +0800

    酷狗搜索器

--- QQMusicSeach.py
+++ QQMusicSeach.py
@@ -6,7 +6,6 @@ from urllib import request, parse
 def getSongList(name):
     name = parse.quote(name)
     url = "https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.song&searchid=68200167999648105&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=" + name + "&g_tk=907445169&loginUin=10670374&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0"
-    print(url)
     res = request.urlopen(url).read().decode('utf-8')
     res = json.loads(res)['data']['song']['list']
     return res
@@ -19,14 +18,15 @@ def main():
 def PrintList(list):
     x = 1
     for n in range(len(list)):
-        name = repr(list[n]['title']).replace("'", '')
+        songname = repr(list[n]['title']).replace("'", '')
         singerlist = list[n]['singer']
         sinnum = len(singerlist)
         if (sinnum > 1):
             num = 0
             for num in range(len(singerlist)):
-                print(singerlist[num]['name'])
+                name = singerlist[num]['name']
                 num += 1
+                print(name)
         else:
             # print(name+"-"+singername)
             x += 1
