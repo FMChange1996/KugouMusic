244484bce46328aa844f2835335998f03dc343b6
FMCchange1996 <2772002035@qq.com>
2018-01-29 22:18:25 +0800

    QQ

--- QQMusicSeach.py
+++ QQMusicSeach.py
@@ -6,8 +6,9 @@ from urllib import request, parse
 def getSongList(name):
     name = parse.quote(name)
     url = "https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.song&searchid=68200167999648105&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=" + name + "&g_tk=907445169&loginUin=10670374&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0"
+    # print(url)
     res = request.urlopen(url).read().decode('utf-8')
-    res = json.loads(res)['data']['song']['list']
+    res = json.loads(res).get('data').get('song').get('list')
     return res
 
 
@@ -15,23 +16,19 @@ def main():
     return input('请输入要搜索歌曲的名字：')
 
 
-def PrintList(list):
+def printList(list):
     x = 1
     for n in range(len(list)):
         songname = repr(list[n]['title']).replace("'", '')
         singerlist = list[n]['singer']
-        sinnum = len(singerlist)
-        if (sinnum > 1):
-            num = 0
-            for num in range(len(singerlist)):
-                name = singerlist[num]['name']
-                num += 1
-                print(name)
+        if len(singerlist) == 1:
+            singname = singerlist[0].get('name')
         else:
-            # print(name+"-"+singername)
-            x += 1
+            for a in singerlist:
+                bb = '/'
+                singname += n.get('name') + bb
+        print(str(x) + "." + songname + "-" + singerlist[0].get('name'))
+        x += 1
 
 
-
-
-PrintList(getSongList(main()))
+printList(getSongList(main()))
